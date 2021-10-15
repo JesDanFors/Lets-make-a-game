1c60fb89e64a9fef7fef53f726ebd81cc08fa000
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-15 14:26:17 +0200

    Added Navmesh for AI movement prep and interactable items

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,21 +5,33 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Movement2 : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public int movementSpeed = 20;
     public float gravity = 20.0f;
     public int blinkDistance = 5;
     private Vector3 _moveDirection = Vector3.zero;
     CharacterController _charControl;
+    private Camera cam;
+    public LayerMask InteractMap;
+    public float interactRange = 10f;
 
     private void Start()
     {
+        cam = Camera.main;
         _charControl = GetComponent<CharacterController>();
     }
     
     // Update is called once per frame
     void Update()
+    {
+        Movement(); // Checks movement input from player.
+        Dash(); // checks every update if player wants to dash.
+        Interact(); // Checks if the player interacts with anything
+
+    }
+    //Movement of the player
+    private void Movement()
     {
         if (_charControl.isGrounded)
         {
@@ -30,12 +42,26 @@ public class Movement2 : MonoBehaviour
         //general movement
         _moveDirection.y -= gravity * Time.deltaTime;
         _charControl.Move(_moveDirection * Time.deltaTime);
-        
-        //Dash ability
+    }
+    //Dash ability
+    private void Dash()
+    {
         if (Input.GetKey(KeyCode.LeftShift))
         {
             _charControl.transform.Translate(_moveDirection*Time.deltaTime * blinkDistance);
         }
-        
+    }
+    //Checks if player Interacts with its environment
+    void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, interactRange,InteractMap))
+            {
+                Debug.Log($"We interacted with a {hit.collider.name} at {hit.point}");
+            }
+        }
     }
 }
